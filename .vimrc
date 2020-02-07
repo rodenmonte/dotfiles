80604fd7d639006ffdf5812e80cab71da59246a4
Monte Roden <rodenmonte@gmail.com>
2020-02-07 03:33:43 +0000

    Add structure for vimfiles

---
+++ .vimrc
@@ -0,0 +1,7 @@
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+imap kj <Esc>
+set cursorline
+set number
+color ron
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TODO Make re-runnable
+ln -s ./.vim ~/.vim
+ln -s ./.tmux.conf ~/.tmux.conf
+ln -s ./.vimrc ~/.vimrc
